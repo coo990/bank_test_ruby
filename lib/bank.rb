d9267786631858611c6807fb58a40b6a66f4d6e3
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-08-03 11:39:49 +0100

    Hardcoded deposit and withdraws test passing

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,10 +1,17 @@
 class Bank
-  def balance
-    0
+  attr_reader :balance
+  def initialize
+    @balance = 0
   end
 
-  def deposit(amount)
-    amount
+  def deposit(amount, time)
+    @balance += amount
+    puts "#{time} || #{amount} || #{balance}"
+  end
+
+  def withdraw(amount, time)
+    @balance -= amount
+    puts "#{time} || #{amount} || #{balance}"
   end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,14 +1,22 @@
 require 'bank'
 
 describe Bank do
+  let(:bank) { Bank.new }
+
   it 'has a balance of 0' do
-    bank = Bank.new
     expect(bank.balance).to eq(0)
   end
 
-  it 'adds 1000 to balance' do
-    bank = Bank.new
-    expect(bank.deposit(1000)).to eq(1000)
+  it 'adds 1000 to balance on 10-01-2012' do
+    bank.deposit(1000, "10-01-2012")
+    expect(bank.balance).to eq(1000)
   end
+
+  it 'withdraws 500 from balance on 12-01-2012' do
+    bank.deposit(1000, "10-01-2012")
+    bank.withdraw(500, "12-01-2012")
+    expect(bank.balance).to eq(500)
+  end
+  
 
 end
